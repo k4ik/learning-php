56137d2bdc1cc3dd7c6e3485f77307b1bda2a210
k4ik <kaikleal1234@gmail.com>
2024-01-31 17:00:11 -0300

    feat: form(calculator)

---
+++ src/operators/calculator.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post">
+        <label for="value">Enter a value: </label>
+        <input type="number" placeholder="4" name="value">
+
+        <select name="" id="">
+            <option value="">Select a option</option>
+            <option value="addition">+</option>
+            <option value="subtraction">-</option>
+            <option value="multiplication">*</option>
+            <option value="division">/</option>
+        </select>
+
+        <label for="value">Enter another value: </label>
+        <input type="number" placeholder="2" name="anotherValue">
+
+        <button type="submit">Calculate</button>
+    </form>
+</body>
+</html>
