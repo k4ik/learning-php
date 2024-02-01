de79125818ecaeb0a913029e882d7ae73f70e0ed
k4ik <kaikleal1234@gmail.com>
2024-02-01 15:59:31 -0300

    feat: logic

--- src/conditionals/credit-card-validation/credit-card-validation.php
+++ src/conditionals/credit-card-validation/credit-card-validation.php
@@ -7,11 +7,43 @@
 </head>
 <body>
     <main>
-        <h1>Credit Card Validator</h1>
-        <form method="post">
-            <input type="text" placeholder="1234567890123456">
-            <button type="submit">Submit</button>
-        </form>
+        <div class="container">
+            <h1>Credit Card Validator</h1>
+            <form method="post">
+                <input type="text" placeholder="1234567890123456" name="cardNumber">
+                <button type="submit">Submit</button>
+            </form>
+            <?php
+                if ($_SERVER["METHOD_REQUEST"] == "") {
+                    $cardNumber = $_POST["cardNumber"];
+                    if(validationCardNumber($cardNumber)) {
+                        echo "the card number is valid";
+                    } else {
+                        echo "the card number is invalid";
+                    }
+                } else {
+                    echo "Enter your card number so we can validate";
+                }
+                function validationCardNumber($cardNumber) {
+                    $cardNumber = str_replace(array(" ", "-"), " ", $cardNumber);
+                    $cardNumber = strrev($cardNumber);
+                    $sum = 0;
+                    for($i = 0, $lenght = strlen($cardNumber); $i < $lenght; $i++) {
+                        $digit = (int)$cardNumber[$i];
+                        if ($i % 2 === 0) {
+                            $sum += $digit;
+                        } else {
+                            $digit *= 2;
+                            if ($digit > 9) {
+                                $digit -= 9;
+                            }
+                            $sum += $digit;
+                        }
+                    }
+                    return $sum % 10 === 0;
+                }
+            ?>
+        </div>
     </main>
 </body>
 </html>
