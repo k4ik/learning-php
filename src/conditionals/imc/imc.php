f7270ab6b34a2e5e91d2e1c6f8b50dd035e9c715
k4ik <kaikleal1234@gmail.com>
2024-02-01 12:37:55 -0300

    feat: logic (imc)

--- src/conditionals/imc/imc.php
+++ src/conditionals/imc/imc.php
@@ -7,19 +7,59 @@
 </head>
 <body>
     <main>
-        <form>
+        <form method="post" class="form">
             <label for="">Enter your height(m):</label>
-            <input type="text" placeholder="1.87">
+            <input type="text" placeholder="1.87" name="height">
 
             <label for="">Enter your weight(kg):</label>
-            <input type="text" placeholder="89.3">
+            <input type="text" placeholder="89.3" name="weight">
 
             <button type="submit">Calculate your BMI</button>
         </form>
 
         <div class="result">
             <?php 
-                # imc = peso / (altura * altura)
+                if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $height = $_POST["height"];
+                    $weight = $_POST["weight"];
+                    $height = (float) $height;
+                    $weight = (float) $weight;
+
+                    $bmi = number_format(getBMI($height, $weight), 2, ".", "");
+                    echo "<p class='bmi'>BMI: $bmi</p>";
+
+                    $status = getStatus($bmi);
+                    echo "<p class='status'>STATUS: $status</p>";
+                } else {
+                    echo "invalid request method";
+                }
+
+                function getBMI(float $height, float $weight) {
+                    $bmi = $weight / ($height * $height);
+                    return $bmi;
+                }
+
+                function getStatus(float $bmi) {
+                    $status = null;
+
+                    if ($bmi < 18.5) {
+                        $status = "Underweight";
+                    } elseif ($bmi >= 18.5 && $bmi < 24.9) {
+                        $status = "Normal weight";
+                    } elseif ($bmi >= 25.0 && $bmi < 29.9) {
+                        $status = "Overweight";
+                    } elseif ($bmi >= 30.0 && $bmi < 34.9) {
+                        $status = "Obesity (Class I)";
+                    } elseif ($bmi >= 35.0 && $bmi < 39.9) {
+                        $status = "Obesity (Class II)";
+                    } elseif ($bmi >= 40.0) {
+                        $status = "Obesity (Class III)";
+                    } else {
+                        echo "Error.";
+                    }
+
+                    return $status;
+                }
             ?>
         </div>
     </main>
