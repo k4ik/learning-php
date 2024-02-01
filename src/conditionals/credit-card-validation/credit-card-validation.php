4af380927d56681663eb77cbdb1b4bac234d9cf2
k4ik <kaikleal1234@gmail.com>
2024-02-01 15:44:22 -0300

    feat: structure html

---
+++ src/conditionals/credit-card-validation/credit-card-validation.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        <h1>Credit Card Validator</h1>
+        <form method="post">
+            <input type="text" placeholder="1234567890123456">
+            <button type="submit">Submit</button>
+        </form>
+    </main>
+</body>
+</html>
