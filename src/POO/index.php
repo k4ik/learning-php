1029824054f83f9e5b77bfb6ce670cdfe86f2974
k4ik <kaikleal1234@gmail.com>
2024-02-15 17:11:29 -0300

    feat: POO - ex2

--- src/POO/index.php
+++ src/POO/index.php
@@ -1,18 +1,32 @@
 <?php 
-    class Product {
-        private $description;
-        private $price;
+    class Fabricante {
+        private $name;
 
-        public function __construct($description, $price) {
-            $this->description = $description;
-            $this->price = $price;
+        public function __construct($name) {
+            $this->name = $name;
         }
 
-        public function getDetails() {
-            return "Description: {$this->description}<br>Price: {$this->price}";
+        public function getName() {
+            return $this->name;
         }
     }
 
-    $product = new Product("Book", "35");
+    class Product {
+        private $description;
+        private $price;
+        private $fabricante;
+
+        public function __construct($description, $price, Fabricante $fabricante) {
+            $this->description = $description;
+            $this->price = $price;
+            $this->fabricante = $fabricante;
+        }
+
+        public function getDetails() {
+            return "Description: {$this->description}<br>Price: {$this->price}<br>Fabricante: {$this->fabricante->getName()}";
+        }
+    }
+    $fabricante1 = new Fabricante('Editora B');
+    $product = new Product("Book", "35", $fabricante1);
     echo $product->getDetails();
 ?>
